693e286ef7369fe633da1a2c688bbfbc165fd182
Hasala <hasaladissanayake@yahoo.com>
2023-07-15 22:58:34 +0530

    add jwt builder and claims methods

--- backend/src/main/java/com/medease/backend/service/JwtService.java
+++ backend/src/main/java/com/medease/backend/service/JwtService.java
@@ -1,4 +1,39 @@
 package com.medease.backend.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "dd61ea8f9f88026793758cba13c57581035fcd97cc7017adef996e8d73688d93";
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    // to ensure the jwt sender is the one who claims that
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
