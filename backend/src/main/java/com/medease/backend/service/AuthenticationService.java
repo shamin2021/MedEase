63b3a9e2c6603dae0776e127ed07088b8726740d
Hasala <hasaladissanayake@yahoo.com>
2023-07-16 08:04:27 +0530

    authService added

---
+++ backend/src/main/java/com/medease/backend/service/AuthenticationService.java
@@ -0,0 +1,58 @@
+package com.medease.backend.service;
+
+import com.medease.backend.dto.AuthenticationRequest;
+import com.medease.backend.dto.AuthenticationResponse;
+import com.medease.backend.dto.RegisterRequest;
+import com.medease.backend.entity.Role;
+import com.medease.backend.entity.User;
+import com.medease.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+
+    //Always given a static role here (need to change that)
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.VISITOR)
+                .build();
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        // if username and email are correct
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
