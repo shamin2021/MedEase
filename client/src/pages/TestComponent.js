e23ee04de1f95071e58e108381c6adfbde11dd76
Hasala <hasaladissanayake@yahoo.com>
2023-07-17 23:10:03 +0530

    component tests

--- client/src/pages/TestComponent.js
+++ client/src/pages/TestComponent.js
@@ -1,34 +1,32 @@
 import React from 'react'
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 import '../styles/TestComponent.css'
 
 const TestComponent = () => {
 
-  const addColor = '#32a852'
+  // const addColor = '#32a852'
 
   return (
     <div className='TestComponent'>
-      <Stack spacing={2} direction="row">
-        <Button variant="text" className='tex'>Text</Button>
-        <Button variant="contained" size='small'>Contained</Button>
-        <Button variant="outlined" size='medium'>Outlined</Button>
-        <Button variant="contained" disabled>Disabled</Button>
-        <Button variant="outlined" disabled>Disabled</Button>
-        <Button variant="contained" color="success" size='large'>SUCCESS</Button>
-        <Button variant="outlined" style={{backgroundColor: addColor}}>ERROR</Button>
-      </Stack>
+      <Box mt={2}>
 
-    <div class="flex items-center justify-between">
-        <h2 class="font-semibold text-slate-900">Projects</h2>
-        <a href="/new" class="hover:bg-blue-400 group flex items-center rounded-md bg-green-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
-        <svg width="20" height="20" fill="currentColor" class="mr-2" aria-hidden="true">
-          <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-        </svg>
-        New
-      </a>
-    </div>
+        <Stack spacing={2} direction="row">
+          <Button variant="text" className='tex'>Click Me</Button>
+          <Button variant="contained" size='small'>Click Me</Button>
+          <Button variant="outlined" size='medium' startIcon={<DeleteIcon />}>Click Me</Button>
+          <Button variant="contained" disabled>Click Me</Button>
+          <Button variant="outlined" disabled>Click Me</Button>
+          <Button variant="contained" color="success" size='large'>SUCCESS</Button>
+          <Button variant="outlined" color='error' size='small' >ERROR</Button>
+        </Stack>
+        
+      </Box>
+
     </div>
   )
 }
