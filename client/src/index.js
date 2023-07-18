d12c51e480b85d4c8f57d0d728018620d8cea3c5
Hasala <hasaladissanayake@yahoo.com>
2023-07-18 11:13:27 +0530

    added chakra provider and a btn in pages/testcomponent

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,22 @@ import App from './App';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TestComponent from './pages/TestComponent';
 import Register from './pages/Register';
+import { ChakraProvider } from "@chakra-ui/react";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <Router>
-    <Routes>
-      {/* add all the application routes here */}
-      <Route path="/" element={<App />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/test-components" element={<TestComponent />} />
-    </Routes>
-  </Router>
+  // added chakra provider here 
+  <ChakraProvider>
+    <Router>
+      <Routes>
+        {/* add all the application routes here */}
+        <Route path="/" element={<App />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/test-components" element={<TestComponent />} />
+      </Routes>
+    </Router>
+  </ChakraProvider>
+
 );
--- client/src/pages/TestComponent.js
+++ client/src/pages/TestComponent.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import DeleteIcon from '@mui/icons-material/Delete';
 
+import { Button} from '@chakra-ui/react'
 
 import '../styles/TestComponent.css'
 
@@ -13,19 +10,8 @@ const TestComponent = () => {
 
   return (
     <div className='TestComponent'>
-      <Box mt={2}>
 
-        <Stack spacing={2} direction="row">
-          <Button variant="text" className='tex'>Click Me</Button>
-          <Button variant="contained" size='small'>Click Me</Button>
-          <Button variant="outlined" size='medium' startIcon={<DeleteIcon />}>Click Me</Button>
-          <Button variant="contained" disabled>Click Me</Button>
-          <Button variant="outlined" disabled>Click Me</Button>
-          <Button variant="contained" color="success" size='large'>SUCCESS</Button>
-          <Button variant="outlined" color='error' size='small' >ERROR</Button>
-        </Stack>
-        
-      </Box>
+      <Button colorScheme='blue'>Button</Button>
 
     </div>
   )
