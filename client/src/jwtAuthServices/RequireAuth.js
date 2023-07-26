ea448365f4c5f198967e3ab937202b72af1abdaa
Hasala <hasaladissanayake@yahoo.com>
2023-07-26 18:22:21 +0530

    changed login code

--- client/src/jwtAuthServices/RequireAuth.js
+++ client/src/jwtAuthServices/RequireAuth.js
@@ -1,15 +1,14 @@
-import { useLocation, Navigate } from "react-router-dom";
+import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 const RequireAuth = ({ allowedRoles }) => {
     const { auth } = useAuth();
     const location = useLocation();
-    
-    const isRoleAllowed = allowedRoles?.includes(auth?.roles);
+    console.log("RequireAuth: auth: ", auth);
     return (
-        isRoleAllowed
-            ? <Navigate to="/patientdash" state={{ from: location }} replace />
-            : auth?.user
+        allowedRoles?.includes(auth?.roles)
+            ? <Outlet />
+            : auth?.email
                 ? <Navigate to="/unauthorized" state={{ from: location }} replace />
                 : <Navigate to="/login" state={{ from: location }} replace />
     );
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-import { useRef, useState, useEffect } from 'react';
-import useAuth from '../hooks/useAuth';
-import { LoginUser } from '../services/AuthService';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import '../styles/Register.css';
+
+import useAuth from "../hooks/useAuth";
+import axios from '../constants/axios';
 
 
 const Login = () => {
@@ -12,79 +12,90 @@ const Login = () => {
 
     const navigate = useNavigate();
     const location = useLocation();
-    const from = location.state?.from?.pathname || '/patientdash';
+    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const userRef = useRef();
-    const errRef = useRef();
-
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
 
     useEffect(() => {
-        userRef.current.focus();
-    }, []);
-
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pwd]);
-
+        setErrorMsg('');
+    }, [email, password])
 
     const handleSubmit = async (e) => {
+
         e.preventDefault();
 
         try {
-            const roles = await LoginUser(user, pwd);
-            console.log(roles);
-            setAuth({ user, pwd, roles });
-            setUser('');
-            setPwd('');
+
+            const response = await axios.post('/auth/authenticate',
+                JSON.stringify({ email, password }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    // withCredentials: true
+                }
+            );
+            const roles = response?.data?.role;
+            setAuth({ email, password, roles });
+            setEmail('');
+            setPassword('');
+
+            const from = location.state?.from || { pathname: '/patientdash'};
             navigate(from, { replace: true });
+
         } catch (err) {
-            setErrMsg(err.message);
-            errRef.current.focus();
+            if (!err?.response) {
+                setErrorMsg('No Server Response');
+            } else if (err.response?.status === 400) {
+                setErrorMsg('Missing Username or Password');
+            } else if (err.response?.status === 403) {
+                setErrorMsg('Unauthorized');
+            } else {
+                setErrorMsg('Login Failed');
+            }
         }
     }
 
     return (
-        <div className='login'>
-            
-                <section>
-                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}
-                        aria-live="assertive">{errMsg}</p>
-                    <h1>Sign In</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            ref={userRef}
-                            autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
-                            value={user}
-                            required
-                        />
 
-                        <label htmlFor="password">Password:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}
-                            required
-                        />
-                        <button>Sign In</button>
-                    </form>
-                    <p>
-                        Need an Account?<br />
-                        <span className="line">
-                            <Link to="/register">Sign Up</Link>
-                        </span>
-                    </p>
-                </section>
+        <div className='login'>
+
+            <section>
+                <p className={errorMsg ? "errorMsg" : "offscreen"} aria-live="assertive">{errorMsg}</p>
+                <h1>Sign In</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="email">Username:</label>
+                    <input
+                        type="text"
+                        id="email"
+                        autoComplete="off"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        required
+                    />
+
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        required
+                    />
+                    <button>Sign In</button>
+                </form>
+                <p>
+                    Need an Account?<br />
+                    <span className="line">
+                        <Link to={"/register"}>Sign Up</Link>
+                    </span>
+                </p>
+            </section>
 
         </div>
+
     )
+
 }
 
 export default Login
