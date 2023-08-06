322836d7f9619cf5c66892a65a8ac4d33de273af
Shamin Fernando <hhshaminf@gmail.com>
2023-08-06 13:29:41 +0530

    commit-searchDoctors

--- client/src/pages/SearchDoc.js
+++ client/src/pages/SearchDoc.js
@@ -13,12 +13,16 @@ const SearchDoc = () => {
   return (
     <div className="h-screen py-1 bg-primary">
       <div className="md:w-3/4 mx-auto shadow-xl rounded-2xl pb-2 py-1 bg-white mt-9 p-5">
-        <div className="font-nunito m-2 mt-4 font-bold">Search for Doctors</div>
-        <input
-          className="w-1/4 h-[40px] font-nunito text-[15px] rounded-3xl bg-[#f5f5f5]"
-          placeholder="Search..."
-          onChange={(e) => setQuery(e.target.value.toLowerCase())}
-        />
+        <div className="ml-6">
+          <div className="font-nunito m-2 mt-4 font-bold">
+            Search for Doctors
+          </div>
+          <input
+            className="w-2/4 h-[40px] font-nunito text-[15px] rounded-3xl bg-[#f5f5f5]"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value.toLowerCase())}
+          />
+        </div>
         <div>{<Table data={search(Docs)} />}</div>
       </div>
     </div>
