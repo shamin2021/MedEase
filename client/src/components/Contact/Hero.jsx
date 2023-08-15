057ae1bf85195f96efdb55676b6c6fb97463b14a
Hasala <hasaladissanayake@yahoo.com>
2023-08-15 15:16:12 +0530

    chat

--- client/src/components/Contact/Hero.jsx
+++ client/src/components/Contact/Hero.jsx
@@ -169,7 +169,7 @@ export default function ContactFormWithSocialButtons() {
 
                     <InputGroup>
                       <InputLeftElement>
-                        <MdOutlineEmail />
+                        {/* <MdOutlineEmail /> */}
                       </InputLeftElement>
                       <Input type="email" name="email" placeholder="Your Email" />
                     </InputGroup>
