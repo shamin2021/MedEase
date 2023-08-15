211ca5b93bc724cd0b65124e48df18500905e819
oshaniwickramasinghe <84929391+oshaniwickramasinghe@users.noreply.github.com>
2023-08-15 11:27:48 +0530

    login to chat automatically

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,26 +1,14 @@
 import { ChatEngine, getOrCreateChat } from 'react-chat-engine'
 import React, { useState } from 'react'
-
-import LoginForm  from '../pages/Login'
-
-import DoctorSideBar from './Doctor/DoctorSideBar'
-
-
+import useAuth from "../hooks/useAuth";
 
 import '../styles/Chat.css';
 import { GridItem } from '@chakra-ui/react';
 
 const DirectChatPage = () => {
 
-	// localStorage.removeItem('username')
-	// localStorage.removeItem('password')
-
+	const { auth, setAuth } = useAuth();
 	const [username, setUsername] = useState('')
-	//if(!localStorage.getItem('username')) return <LoginForm/>
-	// Check if the user credentials are available in local storage.
-	// const storedUsername = localStorage.getItem('username');
-	// const storedPassword = localStorage.getItem('password');
-	// const isLoggedIn = storedUsername && storedPassword;
 
 	function createDirectChat(creds) {
 		getOrCreateChat(
@@ -39,35 +27,37 @@ const DirectChatPage = () => {
 					onChange={(e) => setUsername(e.target.value)} 
 				/>
 				<button className="buttonnewchat" onClick={() => createDirectChat(creds)}>
-					Create
+					CHAT
 				</button>
 			</div>
 		)
 	}
 
+	function renderIceBreaker(chat) {
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+
 	return (
 		<GridItem colSpan={6}>
-      {/* {isLoggedIn ? ( */}
+
 		<ChatEngine 
-			height='80vh'   //change window height
+			height='80vh'
 			width='300px'
-			// userName='oshani'
-			// userSecret='oshani'
-			// userName={localStorage.getItem('username')}
-            // userSecret={localStorage.getItem('password')}
-			userName="oshani"
-            userSecret="Oshani@123"
+			userName={auth.first_name}
+            userSecret={auth.first_name}
 			projectID='d9ef1868-1085-4ef0-bd6c-36276738e453'
 			renderNewChatForm={(creds) => renderNewChatForm(creds)}
+
+			renderIceBreaker={renderIceBreaker} 
 		/>
 
-		{/* ) : (
-			<LoginForm />
-		  )} */}
 		</GridItem>
 		
 	)
-
 	
 }
 
