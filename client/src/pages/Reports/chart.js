5218e4fe33cf9ca113c40cfb43a05f3316745fe0
oshaniwickramasinghe <84929391+oshaniwickramasinghe@users.noreply.github.com>
2023-09-26 19:32:21 +0530

    report with charts

--- client/src/pages/Reports/chart.js
+++ client/src/pages/Reports/chart.js
@@ -24,6 +24,9 @@ const PDFGenerator = () => {
         "user_id": auth.user_id
     };
 
+    // Store the chart instance
+    const [chartInstance, setChartInstance] = useState(null);
+
     useEffect(() => {
         try {
             get(`/SelfAssessments`, setAssessments);
@@ -35,6 +38,31 @@ const PDFGenerator = () => {
 
     useEffect(() => {
         console.log(assessments);
+
+        // Destroy the previous chart instance if it exists
+        if (chartInstance) {
+            chartInstance.destroy();
+        }
+
+        // Render the chart on the canvas element
+        const ctx = chartCanvasRef.current.getContext('2d');
+        const newChartInstance = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4'],
+                datasets: [
+                    {
+                        label: 'Data Set 1',
+                        data: [10, 20, 15, 30],
+                        backgroundColor: ['red', 'blue', 'green', 'orange'],
+                    },
+                ],
+            },
+        });
+
+        // Store the new chart instance
+        setChartInstance(newChartInstance);
+
     }, [assessments]);
 
     const generatePDF = () => {
@@ -49,15 +77,15 @@ const PDFGenerator = () => {
             const currentDate = new Date().toLocaleString();
             doc.setFontSize(10);
             doc.text(`Page ${pageNumber} - ${currentDate}`, 150, 15);
-            doc.text(`Name: ${loggedInUser.username}`, 10, 25); // Print Name
-            doc.text(`Role: ${loggedInUser.role}`, 70, 25); // Print role
-            doc.text(`User ID: ${loggedInUser.user_id}`, 150, 25); // Print id
+            doc.text(`Name: ${loggedInUser.username}`, 10, 25); 
+            doc.text(`Role: ${loggedInUser.role}`, 70, 25); 
+            doc.text(`User ID: ${loggedInUser.user_id}`, 150, 25); 
         };
 
         // Define table headers and data
         const selfassessmentheaders = [['Index', 'First Name', 'Last Name', 'Created Date', 'Risk']];
         const selfassessmentdata = assessments.map((assessment, index) => [
-            index + 1, //10, 20 + index * 10,
+            index + 1, 
             assessment.firstName,
             assessment.lastName,
             assessment.created_date,
@@ -78,7 +106,6 @@ const PDFGenerator = () => {
 
         // Check if there is enough space on page 1 for the first table
         if (table1Height <= doc.internal.pageSize.getHeight()) {
-            // Add the header to page 1
             addHeader(1);
 
             doc.setFontSize(12);
@@ -88,89 +115,61 @@ const PDFGenerator = () => {
             doc.autoTable({
                 head: selfassessmentheaders,
                 body: selfassessmentdata,
-                startY: 40, // Start Y-position of the first table
+                startY: 40, 
                 margin: { top: 10 },
             });
 
             // Add a page break
             doc.addPage();
 
-            // Add the header to page 2
             addHeader(2);
 
             doc.setFontSize(12);
             doc.text('History of Visits', 10, 35);
-            // Print the second table (Visits) on page 2
             doc.autoTable({
                 head: visitsheaders,
                 body: visitsdata,
-                startY: 40, // Start Y-position of the second table on page 2
+                startY: 40,
                 margin: { top: 10 },
             });
         } else {
 
-            // Add the header to page 1
             addHeader(1);
 
             // There is not enough space on page 1 for the first table, so print it on page 2
             doc.addPage();
 
-            // Add the header to page 2
             addHeader(2);
 
             doc.setFontSize(12);
             doc.text('Self Assessment Data', 10, 35);
-            // Print the first table (Self Assessment) on page 2
             doc.autoTable({
                 head: selfassessmentheaders,
                 body: selfassessmentdata,
-                startY: 40, // Start Y-position of the first table on page 2
+                startY: 40, 
                 margin: { top: 10 },
             });
 
-            // Add a page break
             doc.addPage();
 
-            // Add the header to page 3
             addHeader(3);
 
             doc.setFontSize(12);
             doc.text('History of Visits', 10, 35);
-            // Print the second table (Visits) on page 3
             doc.autoTable({
                 head: visitsheaders,
                 body: visitsdata,
-                startY: 40, // Start Y-position of the second table on page 3
+                startY: 40, 
                 margin: { top: 10 },
             });
         }
 
-         // Add a page break
-            doc.addPage();
+        doc.addPage();
 
-            // Add the header to the new page
-            addHeader(4);
+        addHeader(4);
 
-            doc.setFontSize(12);
-            doc.text('Bar Chart', 10, 35);
-
-        // Render the chart on the canvas element
-        const ctx = chartCanvasRef.current.getContext('2d');
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4'],
-                datasets: [
-                    {
-                        label: 'Data Set 1',
-                        data: [10, 20, 15, 30],
-                        backgroundColor: ['red', 'blue', 'green', 'orange'],
-                    },
-                ],
-            },
-
-            
-        });
+        doc.setFontSize(12);
+        doc.text('Bar Chart', 10, 35);
 
         // Capture the chart as an image
         const chartImageUrl = chartCanvasRef.current.toDataURL('image/jpeg', 1.0);
@@ -181,7 +180,6 @@ const PDFGenerator = () => {
         document.body.appendChild(chartImage); // Append the image to the body for display
 
         // Add the canvas to the PDF
-        //const chartImageUrl = chartCanvasRef.current.toDataURL('image/jpeg', 1.0);
         doc.addImage(chartImageUrl, 'JPEG', 10, 45, 190, 100); // Adjust the position and size as needed
 
         // Save the PDF
@@ -194,10 +192,7 @@ const PDFGenerator = () => {
                 <Button onClick={generatePDF}>Generate PDF</Button>
             </Flex>
             {/* Add a canvas element for the chart */}
-            {/* <canvas ref={chartCanvasRef} style={{ display: 'block' }} /> */}
-            <canvas ref={chartCanvasRef} width={100} height={100} style={{ display: 'block' }} />
-
-
+            <canvas ref={chartCanvasRef} width={400} height={200} style={{ display: 'block' }} />
         </GridItem>
     );
 };
